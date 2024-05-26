1da642e1b423c9de0ad1567f6e990e814a700364
MattiaGuggi <mattiahag@gmail.com>
2024-05-26 12:34:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 document.body.onload = function() {
     setTimeout(function() {
-        document.querySelector('.writing').style.visibility = 'visible';
+        let canvas = document.createElement("canvas");
+        let container = document.getElementsByClassName("button-wrapper")[0];
+        canvas.width = 600;
+        canvas.height = 600;
+      
+        container.appendChild(canvas);
+      
+        let confetti_button = confetti.create(canvas);
+        confetti({
+            particleCount: 150,
+            spread: 150,
+            origin: { y: 0.55 }
+          });
+        confetti_button().then(() => container.removeChild(canvas));
+
+        setTimeout(function() {
+            document.querySelector('.writing').style.visibility = 'visible'
+        }, 230);
     }, 2500);
 };
